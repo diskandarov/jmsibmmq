fd5063edc66592388d707db311c9f72c0571b4f9
Алексей Егоров <egorovaa@c-i-systems.com>
2023-04-29 17:58:36 +0300

    1) Изменена версия Spring Boot Starter Parent, так как в maven репозитории не было той версии, которая был указана ранее 2) Изменен способ внедрения бина 3) Исправлен запуск приложения

--- src/main/java/ru/iskdan/jmsibmmq/JmsibmmqApplication.java
+++ src/main/java/ru/iskdan/jmsibmmq/JmsibmmqApplication.java
@@ -1,26 +1,32 @@
 package ru.iskdan.jmsibmmq;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.messaging.Message;
-import ru.iskdan.jmsibmmq.mq.Receiver;
 
 @SpringBootApplication
-@EnableJms
-public class JmsibmmqApplication {
+@RequiredArgsConstructor
+@Slf4j
+public class JmsibmmqApplication implements CommandLineRunner {
+
+	private final JmsTemplate jmsTemplate;
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(JmsibmmqApplication.class, args);
-		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+		SpringApplication.run(JmsibmmqApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
 		String message = "<req>\n" +
 				"<query>Виктор Иван</query>\n" +
 				"<count>7</count>\n" +
 				"</req>";
-		System.out.println("Sending an email message.");
-		jmsTemplate.convertAndSend("JMSqueue2",message);
+		log.info("Sending an email message");
+		jmsTemplate.convertAndSend("JMSqueue2", message);
 	}
+
 
 }
